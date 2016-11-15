ad7034faacd457cf071216c6c9dc5f1d3e426fda
Rocky Wei <finscn@gmail.com>
2016-11-15 21:29:37 +0800

    update renderer-mode (WIP)

--- example/main.js
+++ example/main.js
@@ -59,7 +59,6 @@ function render(context, timeStep, now) {
     context.fillStyle = "rgba(0,0,0,1)";
     context.fillRect(0, 0, Config.width, Config.height);
 
-    // rootUI.render(context);
     rootUI.render(renderer);
 
 }
