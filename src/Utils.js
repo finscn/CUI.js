728e6c4adabc92c803fd23716a5b8ed6458e02fe
Rocky Wei <finscn@gmail.com>
2017-02-08 04:29:56 +0800

    fix : empty panel

--- src/Utils.js
+++ src/Utils.js
@@ -66,13 +66,13 @@ var CUI = CUI || {};
                     var p1 = value.substring(0, plus);
                     var p2 = value.substring(plus + 1);
                     p1 = Utils.parseValue(p1, relativeValue);
-                    p2 = Utils.parseValue(p2, relativeValue)
+                    p2 = Utils.parseValue(p2, relativeValue);
                     return p1 + p2;
                 } else if ((sub = value.lastIndexOf("-")) > 0) {
                     var p1 = value.substring(0, sub);
                     var p2 = value.substring(sub + 1);
                     p1 = Utils.parseValue(p1, relativeValue);
-                    p2 = Utils.parseValue(p2, relativeValue)
+                    p2 = Utils.parseValue(p2, relativeValue);
                     return p1 - p2;
                 } else if ((percent = value.lastIndexOf("%")) > 0) {
                     value = (parseFloat(value) / 100) * (relativeValue || 0);
